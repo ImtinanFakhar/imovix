d9e537d94186b201aee8fc5090b66aa9079244f9
Imtinan <imtinanfakhar879@gmail.com>
2024-05-23 14:34:10 +0500

    Update VidSrcPlayer.jsx

--- src/components/vidSrcPlayer/VidSrcPlayer.jsx
+++ src/components/vidSrcPlayer/VidSrcPlayer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./style.css"; // Make sure to include relevant styles
+import "./style.scss"; // Make sure to include relevant styles
 
 const VidSrcPlayer = ({ mediaType, id }) => {
     const url = `https://vidsrc.me/embed/${mediaType}/${id}`;
