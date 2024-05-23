f7ac19fe10201dc0538e895888c1caf2ed10ea3b
Imtinan <imtinanfakhar879@gmail.com>
2024-05-23 23:07:21 +0500

    Update VidSrcPlayer.jsx

--- src/components/vidSrcPlayer/VidSrcPlayer.jsx
+++ src/components/vidSrcPlayer/VidSrcPlayer.jsx
@@ -3,27 +3,28 @@ import PropTypes from "prop-types";
 import "./style.scss"; // Make sure to include relevant styles
 
 const VidSrcPlayer = ({ mediaType, id }) => {
-    const url = `https://vidsrc.to/embed/${mediaType}/${id}`;
+  const url = `https://vidsrc.to/embed/${mediaType}/${id}?autoplay=1&logo=0`;
 
-    return (
-        <div className="vidsrc-player">
-            <h2>Video Player</h2>
-            <div className="video-container">
-                <iframe
-                    src={url}
-                    referrerPolicy="origin"
-                    frameBorder="0"
-                    allowFullScreen
-                    title="VidSrc Player"
-                ></iframe>
-            </div>
-        </div>
-    );
+  return (
+    <div className="vidsrc-player">
+      <h2>Video Player</h2>
+      <div className="video-container">
+        <iframe
+          src={url}
+          referrerPolicy="origin"
+          frameborder="0"
+          allow="autoplay; encrypted-media"
+          allowfullscreen
+          referrerpolicy="origin"
+        ></iframe>
+      </div>
+    </div>
+  );
 };
 
 VidSrcPlayer.propTypes = {
-    mediaType: PropTypes.string.isRequired,
-    id: PropTypes.string.isRequired,
+  mediaType: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default VidSrcPlayer;
