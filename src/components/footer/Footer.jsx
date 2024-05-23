ec7318c7f89cf09ecd5e4167464bf86cd77b0834
Imtinan <imtinanfakhar879@gmail.com>
2024-05-23 22:23:09 +0500

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,6 @@
 import React from "react";
-import {
-    FaFacebookF,
-    FaInstagram,
-    FaTwitter,
-    FaLinkedin,
-} from "react-icons/fa";
 
+import logo from "../../assets/movix-logo.svg";
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 
 import "./style.scss";
@@ -14,6 +9,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <ContentWrapper>
+            
                 <ul className="menuItems">
                     <li className="menuItem">Terms Of Use</li>
                     <li className="menuItem">Privacy-Policy</li>
@@ -21,23 +17,17 @@ const Footer = () => {
                     <li className="menuItem">Blog</li>
                     <li className="menuItem">FAQ</li>
                 </ul>
-                <div className="infoText">
-                  Thanks for visiting! Enjoy exploring over 100,000 movies, TV shows, and celebrity profiles—absolutely free and ad-free.
-                </div>
-                <div className="socialIcons">
-                    <span className="icon">
-                        <FaFacebookF />
-                    </span>
-                    <span className="icon">
-                        <FaInstagram />
-                    </span>
-                    <span className="icon">
-                        <FaTwitter />
-                    </span>
-                    <span className="icon">
-                        <FaLinkedin />
-                    </span>
-                </div>
+               
+                <div className="infoSection">
+        <div className="logoContainer">
+        <img src={logo} alt="" />
+        </div>
+        <div className="infoText">
+            Thanks for visiting! Enjoy exploring over 10000 movies, TV shows, and celebrity profiles—absolutely free and ad-free.
+              <h4> ©  IMovix.</h4>
+        </div>
+    </div>
+    
             </ContentWrapper>
         </footer>
     );
