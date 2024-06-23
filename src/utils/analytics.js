148a72b62a13ef917d450d8842ae7f5e9e7680d8
Imtinan <imtinanfakhar879@gmail.com>
2024-06-23 12:20:53 +0500

    Update analytics.js

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -5,6 +5,5 @@ export const initGA = () => {
 };
 
 export const logPageView = () => {
-  ReactGA.set({ page: window.location.pathname + window.location.search });
-  ReactGA.pageview(window.location.pathname + window.location.search);
+  ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
 };
