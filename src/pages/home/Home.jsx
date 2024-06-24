4b7a9412e270b8590bed0f50f8ebf8bc56010eec
Imtinan <imtinanfakhar879@gmail.com>
2024-06-24 20:08:19 +0500

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,7 +9,6 @@ const Home = () => {
     <div className="homePage">
       <Helmet>
         <title>Home - Free Movie Streaming Website</title>
-<meta name="google-adsense-account" content="ca-pub-8919482185936104">
       <meta property="og:title" content="Movie Streaming Website - Home" />
       <meta
         property="og:description"
