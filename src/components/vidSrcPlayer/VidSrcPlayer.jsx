c0dad5e297a17ef4836bd884d6b1f59fd1aeb5ad
Imtinan <imtinanfakhar879@gmail.com>
2024-07-05 17:41:03 +0500

    Update VidSrcPlayer.jsx

--- src/components/vidSrcPlayer/VidSrcPlayer.jsx
+++ src/components/vidSrcPlayer/VidSrcPlayer.jsx
@@ -1,23 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import "./style.scss"; // Make sure to include relevant styles
 
-const VidSrcPlayer = ({ mediaType, id, season, episode }) => {
-  let url;
-  if (mediaType === "movie") {
-    url = `https://vidsrc.me/embed/movie/${id}?autoplay=1&logo=0`;
-  } else if (mediaType === "tv") {
-    url = `https://vidsrc.me/embed/tv/${id}/${season}/${episode}?autoplay=1&logo=0`;
+const serverUrls = {
+  server1: (mediaType, id, season, episode) => {
+    if (mediaType === "movie") {
+      return `https://vidsrc.me/embed/movie/${id}?autoplay=1&logo=0`;
+    } else if (mediaType === "tv") {
+      return `https://vidsrc.me/embed/tv/${id}/${season}/${episode}?autoplay=1&logo=0`;
+    }
+  },
+  server2: (mediaType, id, season, episode) => {
+    if (mediaType === "movie") {
+      return `https://vidsrc.to/embed/movie/${id}?autoplay=1&logo=0`;
+    } else if (mediaType === "tv") {
+      return `https://vidsrc.to/embed/tv/${id}/${season}/${episode}?autoplay=1&logo=0`;
+    }
+  },
+  server3: (mediaType, id, season, episode) => {
+    if (mediaType === "movie") {
+      if (id.startsWith("tt")) { // Check if id is IMDb id
+        return `https://multiembed.mov/directstream.php?video_id=${id}`;
+      } else { // Otherwise assume it's TMDB id
+        return `https://multiembed.mov/directstream.php?video_id=${id}&tmdb=1`;
+      }
+    } else if (mediaType === "tv") {
+      if (id.startsWith("tt")) { // Check if id is IMDb id
+        return `https://multiembed.mov/directstream.php?video_id=${id}&s=${season}&e=${episode}`;
+      } else { // Otherwise assume it's TMDB id
+        return `https://multiembed.mov/directstream.php?video_id=${id}&tmdb=1&s=${season}&e=${episode}`;
+      }
+    }
+  },
+  server4: (mediaType, id, season, episode) => {
+    if (mediaType === "movie") {
+      return `https://player.smashy.stream/movie/${id}`;
+    } else if (mediaType === "tv") {
+      return `https://player.smashy.stream/tv/${id}?s=${season}&e=${episode}`;
+    }
   }
+};
+
+const VidSrcPlayer = ({ mediaType, id, season, episode }) => {
+  const [selectedServer, setSelectedServer] = useState("server1");
+
+  const url = serverUrls[selectedServer](mediaType, id, season, episode);
 
   return (
     <div className="vidsrc-player">
+      <div className="server-selector">
+        {Object.keys(serverUrls).map(server => (
+          <button
+            key={server}
+            className={`server-button ${server === selectedServer ? "active" : ""}`}
+            onClick={() => setSelectedServer(server)}
+          >
+            {server}
+          </button>
+        ))}
+      </div>
       <div className="video-container">
         <iframe
           src={url}
           referrerPolicy="origin"
           allow="autoplay; encrypted-media"
-          allowFullScreen={true}
+          allowFullScreen
         ></iframe>
       </div>
     </div>
