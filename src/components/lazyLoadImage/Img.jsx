1cc7fbeae2dedfa8534399b1ecf05c79c81b2b4d
Imtinan <imtinanfakhar879@gmail.com>
2024-07-12 01:10:59 +0500

    Update Img.jsx

--- src/components/lazyLoadImage/Img.jsx
+++ src/components/lazyLoadImage/Img.jsx
@@ -2,18 +2,13 @@ import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
-const Img = ({ src, className }) => {
-    // Check if src is a string, if not, default to an empty string
-    const validSrc = typeof src === 'string' ? src : '';
-
-    return (
-        <LazyLoadImage
-            className={className || ""}
-            alt=""
-            effect="blur"
-            src={validSrc}
-        />
-    );
-};
+const Img = ({ src = '', alt = '', className = '', lazyEffect = 'blur' }) => (
+    <LazyLoadImage
+        className={className}
+        alt={alt}
+        effect={lazyEffect}
+        src={typeof src === 'string' ? src : ''}
+    />
+);
 
 export default Img;
