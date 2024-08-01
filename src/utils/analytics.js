05f371ae8bc352bcf5fc1906ffc8db6daf9c9f03
Imtinan <imtinanfakhar879@gmail.com>
2024-08-01 17:37:45 +0500

    Delete src/utils/analytics.js

--- src/utils/analytics.js
+++
@@ -1,9 +0,0 @@
-import ReactGA from 'react-ga4';
-
-export const initGA = () => {
-  ReactGA.initialize('G-7ZTDD6T4X0'); // Replace with your tracking ID
-};
-
-export const logPageView = () => {
-  ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
-};
